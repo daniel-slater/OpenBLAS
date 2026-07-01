563e401df706132d3de8d4252b1d439c54a9a252
Daniel R. Slater <109969471+daniel-slater@users.noreply.github.com>
2026-07-01 17:56:19 -0400

    - Updated getarch.c so that a FORCE_xxx section can `#define FORCE_NUM_CORES <number>` to force `get_num_cores()` to return the correct number of cores when cross-compiling - Added `#define FORCE_NUM_CORES 4` to the `FORCE_IMX6Q` section of getarch.c - Added `#define FORCE_NUM_CORES 1` to the `FORCE_IMX6ULL` section of getarch.c

--- getarch.c
+++ getarch.c
@@ -1230,6 +1230,7 @@ USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
        "-DHAVE_VFPV3 -DHAVE_VFP -DHAVE_NEON"
 #define LIBNAME   "imx6q"
 #define CORENAME  "IMX6Q"
+#define FORCE_NUM_CORES 4
 #else
 #endif
 
@@ -1245,6 +1246,7 @@ USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
        "-DHAVE_VFPV3 -DHAVE_VFP -DHAVE_NEON"
 #define LIBNAME   "imx6q"
 #define CORENAME  "IMX6Q"
+#define FORCE_NUM_CORES 1
 #else
 #endif
 
@@ -2007,6 +2009,10 @@ USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 static int get_num_cores(void) {
 
+#if defined(FORCE_NUM_CORES) && FORCE_NUM_CORES > 0
+       return FORCE_NUM_CORES;
+#else
+
   int count;
 #ifdef OS_WINDOWS
   SYSTEM_INFO sysinfo;
@@ -2044,6 +2050,7 @@ static int get_num_cores(void) {
 
 #else
   return 2;
+#endif
 #endif
 }
 
